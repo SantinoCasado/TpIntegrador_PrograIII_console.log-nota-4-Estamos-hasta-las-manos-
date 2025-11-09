1f95aa0da326d383c3c131d76e704c20575f59f7
thiago_ferlado <123381056+thiaguitofernandez@users.noreply.github.com>
2025-11-09 16:31:18 -0300

    clase productos

---
+++ backend/clase producto/producto.js
@@ -0,0 +1,14 @@
+class Producucto{
+    nombre;
+    precio;
+    imagen;
+    tipo;
+
+
+    constructor(nombre, precio, imagen, tipo){        this.nombre = nombre;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.tipo = tipo;
+    }
+    
+}
